da2c4ffd67aed9c61056dba37a59c50b32638052
Kevin Adrian <kevin19305.ib@gmail.com>
2026-01-28 19:52:33 +0700

    Konfigurasi Database (Sequelize)

---
+++ config/database.js
@@ -0,0 +1,11 @@
+const Sequelize = require('sequelize');
+require('dotenv').config();
+
+const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT, // <--- Tambahkan baris ini
+    dialect: 'mysql',
+    logging: false
+});
+
+module.exports = db;
